66e6e211714383b1dece99677468aa451813710b
Lucas Jesus <cc19372@g.unicamp.br>
2020-04-23 21:55:25 -0300

    create a simple list on Android

--- app/app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] alunos = {"Lucas","David","Matheus","Miguel"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lvAlunos);
+        ArrayAdapter<String> adpter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,alunos);
+        listaAlunos.setAdapter(adpter);
     }
 }
