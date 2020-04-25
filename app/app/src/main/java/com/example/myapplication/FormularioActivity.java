4ab91128a3e0a82daae38cea18ba30dca3ba9f33
Lucas Jesus <cc19372@g.unicamp.br>
2020-04-25 11:54:52 -0300

    creating a event and when button is press and understand how happen

--- app/app/src/main/java/com/example/myapplication/FormularioActivity.java
+++ app/app/src/main/java/com/example/myapplication/FormularioActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = (Button)findViewById(R.id.btnSalvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this,"Botão Clicado",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
