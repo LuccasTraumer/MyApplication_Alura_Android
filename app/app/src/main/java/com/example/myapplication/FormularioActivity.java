8999f3c0d10d1d87b1e9b9397457e25017ff754e
Lucas Jesus <cc19372@g.unicamp.br>
2020-04-29 18:08:28 -0300

    created a item on menu and associated a event

--- app/app/src/main/java/com/example/myapplication/FormularioActivity.java
+++ app/app/src/main/java/com/example/myapplication/FormularioActivity.java
@@ -1,11 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -14,14 +20,23 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botaoSalvar = (Button)findViewById(R.id.btnSalvar);
+    }
 
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflate = getMenuInflater();
+        inflate.inflate(R.menu.menu_formulario,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this,"Botão Clicado",Toast.LENGTH_LONG).show();
                 finish();
-            }
-        });
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
